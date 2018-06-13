2169cdf3ef26c88fbee863aae766e47bd3fdc96d
zacharyforreal <36243126+zacharyforreal@users.noreply.github.com>
2018-06-13 14:37:44 -0700

    Add files via upload

--- web120/contactme.php
+++ web120/contactme.php
@@ -1,23 +1,106 @@
-
-    
 <?php include "includes/header.php"?>
 <!--END header.php include here!-->
+
+
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
     
-    
-<!-- Responsive tables coming soon! -->
- 
-<p>we'll attempt iframing in a google doc that shows our final projest wireframe and flowchart here</p>
-</section>
-<!-- END LEFT COL -->
-<!-- START RIGHT COL -->
-<aside>
- <h3>Get help from Xing "Zack" Li</h3>
- <ul>
-     <li>Hours of availability</li>
-     <li>I can help you with the following: New and Redesigned basic websites, WordPress, PHP,...</li>
-     </ul>
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL"; 
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+<section>
+<h2>Any questions?</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  
+  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+    </section>
+
+<aside><h2>Disclaimer</h2>
+<p>Xing Li has made reasonable efforts to ensure the accuracy of the information throughout this website. However, Xing Li reserves the right to make appropriate changes in information found on this website.<br><br>
+
+Nothing contained in this website shall be construed as contractual rights. Xing Li has many links to other websites. These include links to websites operated by other government agencies, nonprofit organizations and private businesses.  Xing li does not endorse any content or linked materials found on websites or web services provided by and/or developed by Xing Li. <br><br>
+
+When linked to another site, you are no longer on the Xing Li website and this Privacy Notice will not apply. When linked to another website, you are subject to the privacy policy of that new site. Xing Li does not warrant the accuracy, reliability or timeliness of any information published by this system, nor endorses any content, viewpoints, products, or services linked from this system, and shall not be held liable for any losses caused by reliance on the accuracy, reliability or timeliness of such information. Portions of such information may be incorrect or not current. <br><br>
+
+Any person or entity that relies on any information obtained from this system does so at their own risk.<br><br>
+</p>
 </aside>
-<!-- END RIGHT COL -->
- 
+
+<?php
+
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+
+?>
+
 <?php include "includes/footer.php"?>
 <!--END footer.php include here!-->
